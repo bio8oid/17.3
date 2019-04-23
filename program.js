f529b25c240d1473a081f879878bf52871a3e495
bio8oid <8bollod8@mail.co.uk>
2019-04-24 01:01:30 +0200

    Add OS info feature

--- program.js
+++ program.js
@@ -1,7 +1,10 @@
+var os = require('os');
+
 process.stdin.setEncoding('utf-8');
 
 console.log("Please input 'specs' in command line if want to know Node version.");
 console.log("Please input 'follow' if you want to see 'process.env' result.");
+console.log("Please input '/getOSinfo' if you want to see OS info.");
 
 process.stdin.on('readable', function() {
 	var input = process.stdin.read();
@@ -19,9 +22,36 @@ process.stdin.on('readable', function() {
 			case 'follow':
 			console.log(process.env);
 			break;
+			case '/getOSinfo':
+			getOSinfo();
+			break;
 			default:
 			process.stderr.write('Wrong instruction!\n');
 			break;
 		}
 	}
+
+function getOSinfo() {
+    var type = os.type();
+    if(type === 'Darwin') {
+        type = 'OSX';
+    } else if(type === 'Windows_NT') {
+        type = 'Windows';
+    }
+    var release = os.release();
+    var cpu = os.cpus()[0].model;
+    var uptime = os.uptime();
+    var userInfo = os.userInfo();
+    console.log('System:', type);
+    console.log('Release:', release);
+    console.log('CPU model:', cpu);
+    console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
+    console.log('User name:', "Kaźmierz");
+    console.log('Home dir:', "C:");
+}
+
+
+
+
+
 });
